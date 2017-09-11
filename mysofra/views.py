f2169bb2292b857174e116a814011df66168ac2a
ElmirHodzic <ehodzic3@etf.unsa.ba>
2017-09-11 13:02:50 +0200

    Update views.py

--- mysofra/views.py
+++ mysofra/views.py
@@ -101,7 +101,7 @@ class MailList(APIView):
     def post(self, request, format=None):
         serializer = MailSerializer(data=request.data)
         if serializer.is_valid():
-            mail_from = 'team@mysofra.at'#request.data['mail_from'] if 'mail_from' in request.data else 
+            mail_from = 'mysofra.at@gmail.com'#request.data['mail_from'] if 'mail_from' in request.data else 
             mail_to = request.data['mail_to'] if 'mail_to' in request.data else 'order@mysofra.at'
             dic = json.loads(request.data['message']);             
             serializer.save()
